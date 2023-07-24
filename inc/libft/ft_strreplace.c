b8e4b894781887e6e84c928b0218dd76edd2b00a
fmouronh <fmouronh@student.42lisboa.com>
2023-07-24 21:36:15 +0100

    fixing memleaks

--- inc/libft/ft_strreplace.c
+++ inc/libft/ft_strreplace.c
@@ -19,14 +19,14 @@ char	*ft_strreplace(char *str, int index, int torem, char *torepl)
 	int		tail;
 
 	len = ft_strlen(str) + ft_strlen(torepl) - torem + 1;
-	tail = ft_strlen(&str[index + torem - 1]);
-	result = ft_calloc(len, sizeof(char));
+	tail = ft_strlen(&str[index + torem]) + 1;
+	result = ft_calloc(len, sizeof(char)) + 1;
 	if (!result)
 		return (NULL);
 	if (index)
-		ft_strlcpy(result, str, index + 1);
+		ft_strlcpy(result, str, index);
 	if (*torepl)
 		ft_strlcat(result, torepl, ft_strlen(result) + ft_strlen(torepl) + 1);
-	ft_strlcat(result, &str[index + torem], ft_strlen(result) + tail + 1);
+	ft_strlcat(result, &str[index + torem], ft_strlen(result) + tail);
 	return (result);
 }
