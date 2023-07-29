c53746debf6140a6a7a6b40fa2d6e39599ce131d
fmouronh <fmouronh@student.42lisboa.com>
2023-07-29 18:26:15 +0100

    pipex overhaul

--- src/pipex/pipex_exec_builtin.c
+++ src/pipex/pipex_exec_builtin.c
@@ -2,10 +2,21 @@
 
 void	run_builtin(t_cmd *cmd, int i)
 {
+	static int	(*f[8])(t_data **, char **) = (int (*[8])(t_data **, char **))
+		{NULL, &echo, &cd, &pwd, &export_bin, &unset, &env, &exit_bin};
+	int	pid;
+
+	pid = 0;
 	if (!cmd->next && i == 0)
-		//	run in parent
+		get_shell()->status = f[cmd->builtin](shell, &cmd->args[1]);
 	else
 	{
-		//	fork & run
+		pid = fork();
+		if (pid == -1)
+			exit_(-8, get_shell());
+		else if (pid == 0)
+			//	run in chd
+		else
+			//	wait for chd to terminate & exit function
 	}
 }
